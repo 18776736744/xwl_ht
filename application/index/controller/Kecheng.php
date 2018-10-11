353877a48196a79cd352ef179bea3e0c1b0abda3
smith <544715883@qq.com>
2018-10-11 17:54:14 +0800

    kecheng save

---
+++ application/index/controller/Kecheng.php
@@ -0,0 +1,25 @@
+<?php
+namespace app\index\controller;
+use think\Db;
+
+
+class Kecheng extends \think\Controller
+{
+    public function save()
+    {	
+        $id = db('kecheng')->insertGetId([
+            'uid'=>1,
+            'age_range'=>input('age_range'),
+            'class_type'=>input('class_type'),
+            'class_zhidu'=>input('class_zhidu'),
+            'money'=>input('money'),
+            'start_time'=>input('start_time'),
+            'school'=>input('school'),
+            'teacher'=>input('teacher'),
+            'description'=>input('description'),
+            'add_time'=>time()
+        ]);
+        return $id;
+    }
+    
+}
