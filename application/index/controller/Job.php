c5dbef3a49f1430025a01a5feb4cbe91942de115
Ljy <1308812087@qq.com>
2018-10-12 16:05:00 +0800

    all

--- application/index/controller/Job.php
+++ application/index/controller/Job.php
@@ -10,46 +10,63 @@ class Job extends \think\Controller
 
     }
     public function Ajob(){
-        $type = input('type');
+        $data = [
+                    'category'=>input('category'),
+                    'classid'=>input('classid'),
+                    'address'=>input('address'),
+                    'people'=>input('people'),
+                    'money'=>input('money'),
+                    'xueli'=>input('xueli'),
+                    'sex'=>input('sex'),
+                    'xingzhi'=>input('xingzhi'),
+                    'description'=>input('description'),
+                    'has_shebao'=>input('has_shebao'),
+                    'has_gjj'=>input('has_gjj'),
+                ];
+        $type = input('type_');
         $id=input('id');
         $uid=input('uid');
         if ($type=='see') {
             if(!empty($id)){
-            $ajob = db('job')->where('id='.$id)->select();
-            return json($ajob);
+                $ajob = db('job')->where('id='.$id)->select();
+                return json($ajob);
             }
         }else if($type=='update'){
             if(!empty($uid)){
                 $update=db('job')->where(['id'=>$id,'uid'=>$uid])
-                                ->update([
-                                    'category'=>$category,
-                                    'classid'=>$classid,
-                                    'address'=>$address,
-                                    'people'=>$people,
-                                    'money'=>$money
-                                ]);
-                                if($update){
-                                    return "更新成功";
-                                }else{
-                                    return "更新失败";
-                                }
-            }
-        }else if ($type=='add') {
-                $insert=db('job')->update([
-                                    'uid'=>$uid,
-                                    'category'=>$category,
-                                    'classid'=>$classid,
-                                    'address'=>$address,
-                                    'people'=>$people,
-                                    'money'=>$money
-                                ]);
-                if($insert){
-                    return "添加成功";
+                                ->update($data);
+                if($update){
+                    return json("1");
                 }else{
-                    return "添加失败";
+                    return json("2");
                 }
             }
-        
+        }else if ($type=='add') {
+                $data['uid']=$uid;
+                $insert=db('job')->update($data);
+                if($insert){
+                    return json("1");
+                }else{
+                    return json("2");
+                }
+        }else if($type=='my'){
+            if(!empty($uid)){
+                $mytop = db('job')->where('uid='.$uid)->order('id desc')->select();
+                if($mytop){
+                    return json($mytop);
+                }else{
+                    return json("2");
+                }
+            }
+        }else if($type =='delete'){
+            $dele = db('job')->where(['id'=>$id,'uid'=>$uid])->delete();
+            if($dele){
+                return json('1');
+            }else{
+                return json("2");
+            }
+        }
+
     }
     
 }
