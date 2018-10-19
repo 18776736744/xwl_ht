3b4180623a46f72bce8fe76cd2ef232197961df1
Ljy <1308812087@qq.com>
2018-10-19 17:18:49 +0800

    all

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -42,5 +42,23 @@ class Index extends \think\Controller
         }
     	return json($job);
     }
+    public function upload(){
+        // 获取表单上传文件 例如上传了001.jpg
+        $file = request()->file('image');
+        
+        // 移动到框架应用根目录/public/uploads/ 目录下
+        if($file){
+            $info = $file->move(ROOT_PATH . 'public' . DS . 'uploads');
+            if($info){
+
+                $img= $info->getSaveName();
+                
+            }else{
+                // 上传失败获取错误信息
+                echo $file->getError();
+            }
+            return json($img);
+    }
+}
 
 }
